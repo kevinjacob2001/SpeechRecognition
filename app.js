ec9aee49c455a47d92bef35ad0558e6470149faf
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-07-20 16:18:46 +0530

    completed

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const btn=document.querySelector(".talk");
 const content=document.querySelector(".content");
 
+const greetings=[
+    "Hai there!Sweetheart its me Kevin, i am doing well",
+    "Goodafternoon,I am watching a movie with Varsha ",
+    "Goodevening Mrs.Parippuvada philip,What about having a cup of tea?",
+    "Hello Mrs.Aron Yan",
+    "Hello Preyemwadha Its me Kevin! I am doing well, What's up? Is Aaron Yan doing well?",
+ "Helloo chakkarae, i am fine"
+]
+
+const friends=[
+    "Friend,best friend,all time crush,chakkara,application tester...Actually i dont know,All i know is that i just love you!"
+
+]
+
+
+
 
 const SpeechRecognition=window.SpeechRecognition|| window.webkitSpeechRecognition;
 const recognition=new SpeechRecognition();
@@ -14,7 +30,7 @@ recognition.onresult=function(event){
 const current=event.resultIndex;
 const transcript=event.results[current][0].transcript;
 content.textContent=transcript;
-
+readOutLoad(transcript);
 }
 
 //add the listener
@@ -22,3 +38,21 @@ content.textContent=transcript;
 btn.addEventListener("click",()=>{
     recognition.start();
 })
+
+
+function readOutLoad(message){
+    const speech=new SpeechSynthesisUtterance();
+
+    speech.text="I dont know what you said,please repeat!";
+
+    if(message.includes("how are you")){
+const finalText=greetings[Math.floor(Math.random()*greetings.length)]
+speech.text=finalText;
+    }
+
+     
+    speech.volume=3;
+speech.rate=1;
+speech.pitch=2;
+window.speechSynthesis.speak(speech);
+}
